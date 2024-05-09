dbc297beb3e0f94a168235cdeb6a8e48df1824a6
Nelson Lee <leelhn2345@outlook.com>
2024-05-09 14:02:08 +0800

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,46 @@
+import Link from "next/link";
+
 export function Footer() {
   return (
     <footer className="py-6 md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+      <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
         <p
           className="text-balance text-center text-sm leading-loose text-muted-foreground
             md:text-left"
         >
-          footer component
+          Built using{" "}
+          <Link
+            href="https://nextjs.org/"
+            className="font-medium underline underline-offset-4"
+            target="_blank"
+          >
+            Next.js
+          </Link>{" "}
+          and{" "}
+          <Link
+            href="https://github.com/tokio-rs/axum"
+            className="font-medium underline underline-offset-4"
+            target="_blank"
+          >
+            Axum
+          </Link>
+          . The source code is available on{" "}
+          <Link
+            href="#"
+            className="font-medium underline underline-offset-4"
+            target="_blank"
+          >
+            here (client)
+          </Link>{" "}
+          and{" "}
+          <Link
+            href="#"
+            className="font-medium underline underline-offset-4"
+            target="_blank"
+          >
+            here (server)
+          </Link>
+          .
         </p>
       </div>
     </footer>
