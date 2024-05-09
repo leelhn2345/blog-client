c5c5c59542c38ebc87d8c13f1b69806939b8606c
Nelson Lee <leelhn2345@outlook.com>
2024-05-09 14:21:57 +0800

    modify notfound css

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,10 +3,13 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex min-h-[80vh] items-center justify-center ">
+    <div
+      className="container absolute inset-x-0 top-1/2 flex -translate-y-1/2 items-center
+        justify-center"
+    >
       <div className="space-y-4 text-center">
         <h2 className="font-bold">Page Not Found</h2>
-        <p>We{"'"}re sorry, we could not find the requested resource.</p>
+        <p>We{"'"}re sorry, we could not process your request.</p>
         <Button>
           <Link href="/">Return Home</Link>
         </Button>
