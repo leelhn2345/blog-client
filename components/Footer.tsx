789984e59db14d21b88640963f757e6dfe9ce34e
Nelson Lee <leelhn2345@outlook.com>
2024-05-11 22:47:28 +0800

    change footer padding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="py-6 md:px-8 md:py-0">
+    <footer className="py-2 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
           Built using{" "}
