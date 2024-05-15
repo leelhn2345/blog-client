c74de86aa472aa10dc1081df5e6894451fe4c06b
Nelson Lee <leelhn2345@outlook.com>
2024-05-15 21:20:01 +0800

    react query provider

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
           <div className="mb-8 pl-3">
             <Image
               width={200}
-              priority
               className="mx-auto mb-6 rounded-3xl dark:border-2 dark:border-white md:mx-0"
               src={profileImage}
               alt="profile image"
