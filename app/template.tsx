8401760a6d1b83984679bd03a551cc42c9e8197f
Nelson Lee <leelhn2345@outlook.com>
2024-05-21 04:18:34 +0800

    style: page transition with framer motion

---
+++ app/template.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ReactNode } from "react";
+import { motion } from "framer-motion";
+
+type TemplateProps = {
+  children: ReactNode;
+};
+
+export default function Template({ children }: TemplateProps) {
+  return (
+    <motion.div
+      className="flex flex-1"
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ ease: "easeInOut", duration: 1 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
