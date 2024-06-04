1b18e9850cc03c1f64eb57a1c5e03d3a855d3f09
Nelson Lee <leelhn2345@outlook.com>
2024-06-04 15:43:39 +0800

    .

---
+++ app/account_verification/page.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+async function verifyAccount(token: string) {
+  const res = await fetch(
+    `${process.env.BACKEND_URL}/user/sign-up-verification/${token}`,
+    {
+      method: "PUT",
+    },
+  );
+  return res;
+}
+
+type PageProps = {
+  searchParams: { token: string };
+};
+
+export default async function AccountVerificationPage({
+  searchParams,
+}: PageProps) {
+  const res = await verifyAccount(searchParams.token);
+  if (!res.ok) {
+    return (
+      <div className="container mt-10">
+        <p>Sorry, there are errors pending your account verification.</p>
+        <p>Please contact support for help.</p>
+      </div>
+    );
+  }
+  return (
+    <div className="container mt-10">
+      <p>Congrats! You are now verified!</p>
+      <br />
+      you may now{" "}
+      <Link href="/auth">
+        <Button>login</Button>
+      </Link>
+      .
+    </div>
+  );
+}
