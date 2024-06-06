42f2bbd68590ff38ffb3d98eb515117e94932992
Nelson Lee <leelhn2345@outlook.com>
2024-06-07 01:29:05 +0800

    edit card hover

--- components/ui/card-hover-effect.tsx
+++ components/ui/card-hover-effect.tsx
@@ -1,42 +1,40 @@
-"use client"
+"use client";
 import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { HTMLAttributes, useState } from "react";
+import { Card, CardDescription, CardTitle } from "./card";
 
-
-interface Props extends HTMLAttributes<HTMLElement>{
-  items:{
-    title:string;
-    description:string;
-    link:string
-  }[]
+interface Props extends HTMLAttributes<HTMLElement> {
+  items: {
+    title: string;
+    description: string;
+    link: string;
+  }[];
 }
-export const HoverEffect = ({
-  items,
-  className,
-}: Props) => {
+export const HoverEffect = ({ items, className }: Props) => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   return (
     <div
       className={cn(
-        "grid grid-cols-1 var(--) md:grid-cols-2  lg:grid-cols-3  py-10",
-        className
+        "var(--) grid grid-cols-1 py-10 md:grid-cols-2 lg:grid-cols-3",
+        className,
       )}
     >
       {items.map((item, idx) => (
         <Link
           href={item?.link}
           key={item?.link}
-          className="relative group  block p-2 h-full w-full"
+          className="group relative block h-full w-full p-2"
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}
         >
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                className="absolute inset-0 block h-full w-full rounded-3xl bg-neutral-200
+                  dark:bg-slate-800/[0.8]"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
@@ -50,63 +48,20 @@ export const HoverEffect = ({
               />
             )}
           </AnimatePresence>
-          <Card>
-            <CardTitle>{item.title}</CardTitle>
-            <CardDescription>{item.description}</CardDescription>
+          <Card
+            className="relative z-20 h-full w-full overflow-hidden rounded-2xl border
+              border-transparent p-4 group-hover:border-slate-700 dark:border-white/[0.2]
+              dark:bg-black"
+          >
+            <CardTitle className="mt-4 font-bold tracking-wide dark:text-zinc-100">
+              {item.title}
+            </CardTitle>
+            <CardDescription className="mt-8 text-sm leading-relaxed tracking-wide text-zinc-500 dark:text-zinc-400">
+              {item.description}
+            </CardDescription>
           </Card>
         </Link>
       ))}
     </div>
-  );
-};
-export const Card = ({
-  className,
-  children,
-}: {
-  className?: string;
-  children: React.ReactNode;
-}) => {
-  return (
-    <div
-      className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden dark:bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
-        className
-      )}
-    >
-      <div className="relative z-50">
-        <div className="p-4">{children}</div>
-      </div>
-    </div>
-  );
-};
-export const CardTitle = ({
-  className,
-  children,
-}: {
-  className?: string;
-  children: React.ReactNode;
-}) => {
-  return (
-    <h4 className={cn("dark:text-zinc-100 font-bold tracking-wide mt-4", className)}>
-      {children}
-    </h4>
-  );
-};
-export const CardDescription = ({
-  className,
-  children,
-}: {
-  className?: string;
-  children: React.ReactNode;
-}) => {
-  return (
-    <p
-      className={cn(
-        "mt-8 text-zinc-500 dark:text-zinc-400 tracking-wide leading-relaxed text-sm",
-        className
-      )}
-    >
-      {children}
-    </p>
   );
 };
