837c14f7759992837e2ea5a59e0b034776ddf8de
Nelson Lee <64735315+leelhn2345@users.noreply.github.com>
2024-06-07 19:41:28 +0800

    feat: project page (#13)

--- app/about/projects.tsx
+++ app/about/projects.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
 export type TProject = {
-  projectName: string;
-  projectUrl: string;
-  description: string[];
+  name: string;
+  url: string;
+  description?: string[];
 };
 
 type ProjectProp = {
@@ -13,17 +13,17 @@ export function Projects({ data }: ProjectProp) {
   return (
     <div className="mt-4">
       {data.map((x) => (
-        <div key={x.projectName} className="mb-8">
+        <div key={x.name} className="mb-8">
           <Link
-            href={x.projectUrl}
+            href={x.url}
             className="text-base font-semibold hover:text-blue-800 hover:underline
               hover:underline-offset-4"
           >
-            {x.projectName}
+            {x.name}
           </Link>
           <ul className="ml-5 mr-2 mt-4 list-disc space-y-2 text-justify">
-            {x.description.map((feat, num) => (
-              <li key={x.projectName + num}>{feat}</li>
+            {x.description?.map((feat, num) => (
+              <li key={x.name + num}>{feat}</li>
             ))}
           </ul>
         </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,12 @@
-import { HoverEffect } from "@/components/ui/card-hover-effect";
+import { Badge } from "@/components/ui/badge";
+import { HoverEffect } from "@/app/projects/card-hover-effect";
 
-export default function ProjectPage() {
+async function getProjectList() {
+  const res = await fetch(`${process.env.BACKEND_URL}/resume/projects/summary`);
+  return res.json();
+}
+export default async function ProjectPage() {
+  const data = await getProjectList();
   return (
     <div className="container pt-10">
       <h1
@@ -9,45 +15,14 @@ export default function ProjectPage() {
       >
         ~ In Progress ~
       </h1>
-      <HoverEffect items={projects} />
+      <div className="ml-4">
+        <Badge>Member</Badge> means you need to sign up to use it
+        {"'"}s functionalities.
+        <br />
+        <br />
+        You can still enter the link to find out more about the project.
+      </div>
+      <HoverEffect items={data} />
     </div>
   );
 }
-const projects = [
-  {
-    title: "Stripe",
-    description:
-      "A technology company that builds economic infrastructure for the internet.",
-    link: "https://stripe.com",
-  },
-  {
-    title: "Netflix",
-    description:
-      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-    link: "https://netflix.com",
-  },
-  {
-    title: "Google",
-    description:
-      "A multinational technology company that specializes in Internet-related services and products.",
-    link: "https://google.com",
-  },
-  {
-    title: "Meta",
-    description:
-      "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-    link: "https://meta.com",
-  },
-  {
-    title: "Amazon",
-    description:
-      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-    link: "https://amazon.com",
-  },
-  {
-    title: "Microsoft",
-    description:
-      "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
-    link: "https://microsoft.com",
-  },
-];
