c79441e4f1a97281f71472a4ce50ade06dd3dee0
Nelson Lee <leelhn2345@outlook.com>
2024-06-07 08:28:46 +0800

    remove dynamic force static

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,5 @@
 import { ProgressPage } from "@/components/progress-page";
 
-export const dynamic = "force-static";
-
 export default function BlogPage() {
   return <ProgressPage pageName="blog page" />;
 }
