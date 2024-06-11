e640b557b19b396c898bf4ffa2ac5c0782147a59
Nelson Lee <leelhn2345@outlook.com>
2024-06-11 23:39:59 +0800

    fix spelling

--- app/projects/telegram-whisper/tele-msg.tsx
+++ app/projects/telegram-whisper/tele-msg.tsx
@@ -46,7 +46,8 @@ export async function TeleMsg() {
                 </TooltipTrigger>
                 <TooltipContent>
                   <p>
-                    Directions to this feature code will be provided on request.
+                    For verification purposes, directions to this feature code
+                    will be provided on request.
                   </p>
                 </TooltipContent>
               </Tooltip>
