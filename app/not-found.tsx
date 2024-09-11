1f068e049f7de8d3e42b1f97f7a982d34efecfd5
Nelson Lee <leelhn2345@outlook.com>
2024-09-11 12:44:31 +0800

    update not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,8 @@ export default function NotFound() {
     <div className="container flex items-center justify-center">
       <div className="space-y-4 text-center">
         <h2 className="text-2xl font-bold">Page Not Found</h2>
-        <p>We{"'"}re sorry, we could not process your request.</p>
+        <p>We{"'"}re sorry, we could not process your request at the moment.</p>
+        <p>The site is in midst of server upgrade and migration.</p>
         <br />
         <Link href="/">
           <Button>Return Home</Button>
