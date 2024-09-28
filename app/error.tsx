2f21b6f4d7142244b21526ef8970c0a5c1a6f4d0
Nelson Lee <leelhn2345@outlook.com>
2024-09-29 00:48:12 +0800

    update error page with new url

--- app/error.tsx
+++ app/error.tsx
@@ -3,12 +3,13 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-type ErrorProps = {
-  error: Error;
-  reset: () => void;
-};
+// type ErrorProps = {
+//   error: Error;
+//   reset: () => void;
+// };
 
-export default function ErrorPage({ reset }: ErrorProps) {
+export default function ErrorPage() {
+  // { reset }: ErrorProps
   return (
     <div className="container flex flex-col items-center justify-center gap-y-6 text-center">
       <h1
@@ -17,17 +18,23 @@ export default function ErrorPage({ reset }: ErrorProps) {
       >
         Bad Request
       </h1>
-      <p>Sorry, we couldn{"'"}t find the resources you requested.</p>
+      {/* <p>Sorry, we couldn{"'"}t find the resources you requested.</p> */}
+      {/* <div className="flex gap-x-4"> */}
+      {/*   <Button variant="outline" onClick={() => reset()}> */}
+      {/*     Try Again */}
+      {/*   </Button> */}
+      {/*   <Button> */}
+      {/*     <Link href="/">Return Home</Link> */}
+      {/*   </Button> */}
+      {/*   <Button variant="destructive"> */}
+      {/*     <Link href="/contact">Contact Me</Link> */}
+      {/*   </Button> */}
+      {/* </div> */}
+      {<p>Sorry, I am in the midst of transiting to a new site.</p>}
       <div className="flex gap-x-4">
-        <Button variant="outline" onClick={() => reset()}>
-          Try Again
-        </Button>
-        <Button>
-          <Link href="/">Return Home</Link>
-        </Button>
-        <Button variant="destructive">
-          <Link href="/contact">Contact Me</Link>
-        </Button>
+        <Link href="https://digitalrecords.cloud/about">
+          <Button>Let{"'"}s go to my new site!</Button>
+        </Link>
       </div>
     </div>
   );
